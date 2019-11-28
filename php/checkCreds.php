b59d6b88a98d4d59adbc13eb76116e3c1628692d
Elariondakta <prevottheodore@gmail.com>
2019-11-28 15:44:25 +0100

    Error connexion Handler Android APP

---
+++ php/checkCreds.php
@@ -0,0 +1,22 @@
+<?php 
+    $subs = json_decode(file_get_contents("../json/subs.json"), true);
+    if (isset($_GET["user"], $_GET["pass"])) {
+        $users = array_column($subs, "user", "id");
+        if (in_array(strtoupper($_GET["user"]), $users)) {
+            $id = array_keys($users, strtoupper($_GET["user"]))[0];
+            if (password_verify($_GET["pass"], $subs[$id]["pass"])) {
+                return;
+                exit;
+            } else {
+                echo "Le mot de passe rentré n'existe pas.";
+                exit;
+            }
+        } else {
+            echo "Le nom d'utilisateur rentré n'existe pas.";
+            exit;
+        }
+    } else {
+        echo "Erreur lors de la tentative de connexion.";
+        exit;
+    }
+?>
